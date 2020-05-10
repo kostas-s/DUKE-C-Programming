bc4784e95b12455258e90f9451c37e1c0e0f4105
Coursera Learner <nobody@nowhere.nul>
2020-05-10 14:15:30 +0000

    assignment c3 done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,18 +2,55 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
 
-}
-
-int deck_contains(deck_t * d, card_t c) {
+int deck_contains(deck_t * d, card_t c){
+  for (size_t i=0; i<(d->n_cards); i++) {
+    if (((d->cards[i]->value) == c.value) && ((d->cards[i]->suit)==c.suit)) return 1;
+  }
   return 0;
 }
 
-void shuffle(deck_t * d){
+void print_hand(deck_t * hand){
+  for (size_t i=0; i<(hand->n_cards); i++) {
+    print_card(*(hand->cards[i]));
+    printf(" ");
+  }
+  
+  // for (size_t i=0; i< n_cards; i++){
+  //print_card(*(hand).card_t[i]);
+  //printf(" ");
+  //}
+}
 
+void shuffle(deck_t * d){
+  size_t max_rand=d->n_cards;
+  card_t swap_card;
+  size_t num;
+  size_t num2;
+  for (size_t i=0; i<max_rand; i++){
+    num=rand()%max_rand;
+    num2=rand()%max_rand;
+    //   while (num>max_rand){
+    //num/=2;
+    //}
+    // printf("num =%zu ", num);
+    swap_card.value=d->cards[num]->value;
+    swap_card.suit=d->cards[num]->suit;
+    d->cards[num]->value=d->cards[num2]->value;
+    d->cards[num]->suit=d->cards[num2]->suit;
+    d->cards[num2]->value=swap_card.value;
+    d->cards[num2]->suit=swap_card.suit;
+  }
+
+  
 }
 
 void assert_full_deck(deck_t * d) {
-
+  // printf("running assert_full_deck");
+  assert(d->n_cards == 52);
+  for (size_t i=0; i < d->n_cards; i++){
+    assert_card_valid(*(d->cards[i]));
+    assert(deck_contains(d, card_from_num(i))==1);
+  }
+  
 }
