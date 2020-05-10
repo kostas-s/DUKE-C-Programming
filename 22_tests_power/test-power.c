82545ce3ba413e1a73f6e03189e95507555536c4
Coursera Learner <nobody@nowhere.nul>
2020-05-10 11:10:07 +0000

    added modified file

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -17,16 +17,16 @@ int main(){
   if (power (1,0)!=1) exit(EXIT_FAILURE);
   if (power (-1,0)!=1) exit(EXIT_FAILURE);
   if (power (0,0)!=1) exit(EXIT_FAILURE);
-  if (power (100,-3)>1) exit(EXIT_FAILURE);
+  if (power (100,20)>1) exit(EXIT_FAILURE);
       //if (power (100,0)!=1) exit(EXIT_FAILURE);
       // if (power (100,0)!=1) exit(EXIT_FAILURE);
       // if (power (100,0)!=1) exit(EXIT_FAILURE);
 
   for (int i=1; i<300; i++){
-    if (power (i,i/2)!=powCorrect(i,i/2)) {
+     if (power (i,i/2)!=powCorrect(i,i/2)) {
       //      printf("%d to the %d:returned %u, my correct says it should be %u\n",i, i/2, power(i,i/2),powCorrect(i,i/2));
-      exit(EXIT_FAILURE);
-    }
+       exit(EXIT_FAILURE);
+     }
   }
   return EXIT_SUCCESS;
 }
