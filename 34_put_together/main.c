5902b17009d05dd1f4f0eb474453994a02a4c036
Coursera Learner <nobody@nowhere.nul>
2020-05-14 18:19:28 +0000

    34 done

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -4,35 +4,74 @@
 #include "kv.h"
 #include "counts.h"
 #include "outname.h"
+#include <assert.h>
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  FILE *f = fopen(filename, "r");
+  if (f==NULL){
+    fprintf(stderr, "File specified not found");
+    assert(f!=NULL);
+    return NULL;
+  }
+  counts_t * count= createCounts();
+  char * line=NULL;
+  size_t sz=0;
+  char * value = NULL;
+  char * ptr1 = NULL;
+  while (getline(&line, &sz, f)!=EOF){
+    ptr1 = strchr(line, '\n');
+    *ptr1='\0';
+    value = lookupValue(kvPairs, line);
+    addCount(count, value);
+    // printf("did lookup of %s in kvPairs, found %s\n", line, value);
+    //   free(line);
+    }
+  free(line);
+
+  //  printKVs(kvPairs);
+  //printf("%d", kvPairs->list_len);
+  if(fclose(f)!=0){
+    fprintf(stderr, "Unable to close countFile");
+    return NULL;
+  }
+  return count;
 }
 
+
+
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
 
- //count from 2 to argc (call the number you count i)
+  if (argc<3){
+    fprintf(stderr, "Usage: Enter at least 2 files as arguments\n");
+    return EXIT_FAILURE;
+  }
 
-    //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
+  kvarray_t * kv = readKVs(argv[1]);
 
-    //compute the output file name from argv[i] (call this outName)
+  counts_t * c=NULL;
+  char ext[] = ".counts";
+  
+  for (int i=2; i<argc; i++){
+    // printf("OPENING FILE: %s\n", argv[i]);
+    c = countFile(argv[i], kv);
+    //printf("counts_t contains array len:%d unknown:%d\n",c->array_len,c->unknown_count);
 
+    char * outName = malloc(sizeof(* outName)*(strlen(argv[i])+8));
+    strcpy(outName, argv[i]);
+    strcat(outName, ext);
 
-    //open the file named by outName (call that f)
+    FILE * w = fopen(outName, "w");
+    printCounts(c, w);
+    
+    if (fclose(w)!=0){
+      fprintf(stderr, "Unable to close file after writing");
+      return EXIT_FAILURE;
+    }
+    free(outName);
+    freeCounts(c);
+  }
 
-    //print the counts from c into the FILE f
-
-    //close f
-
-    //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+  // freeCounts(c);
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
