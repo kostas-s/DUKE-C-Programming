048dc10937732f45c73afa4476e0bfacd6929341
Coursera Learner <nobody@nowhere.nul>
2020-05-16 01:20:39 +0000

    1

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,6 +3,97 @@
 #include <assert.h>
 #include "deck.h"
 
+void add_card_to(deck_t * deck, card_t c){
+  // create a card and assign c value+suit to it
+  card_t * card = malloc(sizeof(* card));
+  card->value = c.value;
+  card->suit = c.suit;
+
+  // increase size of deck
+  deck->n_cards++;
+  size_t num = deck->n_cards;
+  deck->cards = realloc(deck->cards, num*sizeof(deck->cards));
+
+  // place new card in deck
+  deck->cards[num-1]=card;
+}
+
+card_t * add_empty_card(deck_t * deck){
+  card_t * card = malloc(sizeof(* card));
+  card->value = 0;
+  card->suit = 0;
+
+  deck->n_cards++;
+  size_t num=deck->n_cards;
+  deck->cards = realloc(deck->cards, num*sizeof(deck->cards));
+  deck->cards[num-1]=card;
+  return card;
+}
+
+deck_t * make_deck_exclude(deck_t * excluded_cards){
+  deck_t * deck = malloc(sizeof(* deck));
+  deck->n_cards = 0;
+  deck->cards= NULL;
+  // get num equivalent of cards to exclude
+  int exc_nums[excluded_cards->n_cards];
+  size_t new_size = 52-excluded_cards->n_cards;
+  for (int i=0; i<excluded_cards->n_cards; i++){
+    exc_nums[i] = excluded_cards->cards[i]->value + (excluded_cards->cards[i]->suit*13);
+  }
+  
+  deck->n_cards = new_size;
+  unsigned j=0;
+  for (int i=0; i<new_size; i++){
+
+    for (int x=0; x<excluded_cards->n_cards; x++){
+      if (exc_nums[x]==j) j++;
+      // restart checking in case next number in exc nums is smaller
+      x=0;
+    }
+    card_t ans = card_from_num(j);
+    deck->cards[i]->value = ans.value;
+    deck->cards[i]->suit = ans.suit;
+    j++;
+  }
+
+  return deck;
+
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
+  deck_t * deck = malloc(sizeof(* deck));
+  deck->n_cards = 0;
+  deck->cards = NULL;
+  int pos = 0;
+  for (int h=0; h<n_hands; h++){
+    // for each hands specific size copy cards from hand to deck. hands will be "h" cards in hands will be "c"
+    for (int c=0; c<hands[h]->n_cards; c++){
+      deck->cards=realloc(deck->cards, (sizeof(* deck->cards)*(deck->n_cards+1)));
+      //copy each card from current hand
+      deck->cards[pos]=hands[h]->cards[c];
+      deck->n_cards++;
+      pos++;
+    }
+  }
+  
+  deck_t * ans = make_deck_exclude(deck);
+  free_deck(deck);
+  return ans;
+
+  
+}
+
+void free_deck(deck_t * deck){
+  for (int i=0; i<deck->n_cards; i++){
+    free(deck->cards[i]);
+  }
+  
+  free(deck->cards);
+  free(deck);
+
+}
+
+
 int deck_contains(deck_t * d, card_t c){
   for (size_t i=0; i<(d->n_cards); i++) {
     if (((d->cards[i]->value) == c.value) && ((d->cards[i]->suit)==c.suit)) return 1;
