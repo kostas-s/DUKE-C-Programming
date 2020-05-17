f69b5af16bb2e70ad356e01a8ba5a816949d1a62
Coursera Learner <nobody@nowhere.nul>
2020-05-17 13:09:42 +0000

    docs

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -11,6 +11,24 @@
 
 
 int main(int argc, char ** argv) {
-  //YOUR CODE GOES HERE
+  if (argc > 3 || argc < 2) {
+    fprintf(stderr, "Usage: enter hand and number of trials\n");
+    return EXIT_FAILURE;
+  }
+  
+  // if trial number not entered you should use 10000 as default value
+  unsigned trials = 10000;
+  if (argc==3){
+    trials = atoi(argv[2]);
+  }
+
+  FILE * f = fopen(argv[1], "r");
+  if (f==NULL){
+    fprintf(stderr, "File not found\n");
+    return EXIT_FAILURE;
+  }
+  
+  
+
   return EXIT_SUCCESS;
 }
